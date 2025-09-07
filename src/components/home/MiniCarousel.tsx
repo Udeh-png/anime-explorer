e7cf25aa657219a64eb724d99dfaf2af1405f4a8
Udeh-png <leonwokedichisom@gmail.com>
2025-09-07 18:42:25 +0100

    Enhance MiniCarousel with smooth scrolling and dynamic navigation; refactor loadMore function and add scroll position checks

--- src/components/home/MiniCarousel.tsx
+++ src/components/home/MiniCarousel.tsx
@@ -3,24 +3,96 @@
 import { Card } from "@/components/shared/Card";
 import { getTenTrending } from "@/queries";
 import { PageObject } from "@/types";
-import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaFire } from "react-icons/fa";
 import { LuRefreshCw } from "react-icons/lu";
-import { TbRefresh } from "react-icons/tb";
+import {
+  TbChevronCompactLeft,
+  TbChevronCompactRight,
+  TbRefresh,
+} from "react-icons/tb";
 
 export const MiniCarousel = ({ pageObject }: { pageObject: PageObject }) => {
   const [pageObjectState, setPageObjectState] = useState(pageObject);
+  const [atEnd, setAtEnd] = useState("");
   const [mediaArray, setMediaArray] = useState(pageObject.media);
   const [loading, setLoading] = useState(false);
   const { currentPage, hasNextPage } = pageObjectState.pageInfo;
+  const carrouselRef = useRef<HTMLDivElement>(null);
+
+  function scrollRight() {
+    if (carrouselRef.current) {
+      const carrousel = carrouselRef.current;
+      carrousel.scrollLeft += 500;
+    }
+  }
+
+  function scrollLeft() {
+    if (carrouselRef.current) {
+      const carrousel = carrouselRef.current;
+      carrousel.scrollLeft -= 500;
+    }
+  }
+
+  async function loadMore() {
+    setLoading(true);
+    const newPageObject = await getTenTrending(currentPage + 1);
+    setLoading(false);
+    setPageObjectState(newPageObject);
+    setMediaArray((prev) => [...prev, ...newPageObject.media]);
+    setTimeout(() => {
+      checkScrollPosition();
+    }, 1);
+  }
+
+  function checkScrollPosition() {
+    if (carrouselRef.current) {
+      const carrousel = carrouselRef.current;
+      const scrollLeft = carrousel.scrollLeft;
+      if (scrollLeft <= 0) {
+        setAtEnd("left");
+        return;
+      }
+      if (scrollLeft + carrousel.clientWidth === carrousel.scrollWidth) {
+        setAtEnd("right");
+        return;
+      }
+      setAtEnd("");
+    }
+  }
+
+  useEffect(() => {
+    checkScrollPosition();
+  }, []);
+
   return (
-    <div className="mt-8">
+    <div className="mt-8 overflow-clip">
       <div className="flex py-2 text-4xl gap-2 mb-1.5 text-accent-one ml-8">
         <FaFire />
         <p className="font-semibold">Trending Now</p>
       </div>
-      <div className="overflow-auto mini-carrousel scroll-smooth">
-        <div className="flex gap-x-3 w-fit overflow-auto">
+      <div
+        className="overflow-auto mini-carrousel scroll-smooth -ml-16 -mr-19 relative"
+        onScrollEnd={checkScrollPosition}
+        ref={carrouselRef}
+      >
+        <div className="flex gap-x-3 w-fit">
+          <motion.div
+            className="sticky text-accent-one transition-[background-image] left-16 top-0 flex items-center bottom-0 h-[318px] hover:bg-linear-to-b hover:from-transparent via-black/50 to-transparent text-6xl z-10"
+            animate={
+              atEnd === "left"
+                ? {
+                    opacity: 0,
+                  }
+                : {
+                    opacity: 1,
+                  }
+            }
+            onClick={scrollLeft}
+          >
+            <TbChevronCompactLeft />
+          </motion.div>
           {mediaArray.map((media, i) => {
             return (
               <Card
@@ -35,13 +107,7 @@ export const MiniCarousel = ({ pageObject }: { pageObject: PageObject }) => {
             <div className="w-[230px] h-75 flex items-center justify-center text-accent-one flex-col">
               <div
                 className=" cursor-pointer flex flex-col items-center justify-center"
-                onClick={async () => {
-                  setLoading(true);
-                  const newPageObject = await getTenTrending(currentPage + 1);
-                  setLoading(false);
-                  setPageObjectState(newPageObject);
-                  setMediaArray((prev) => [...prev, ...newPageObject.media]);
-                }}
+                onClick={loadMore}
               >
                 <LuRefreshCw
                   className={`text-6xl ${loading ? "animate-spin" : ""}`}
@@ -50,6 +116,21 @@ export const MiniCarousel = ({ pageObject }: { pageObject: PageObject }) => {
               </div>
             </div>
           )}
+          <motion.div
+            className="sticky text-accent-one transition-[background-image] right-19 top-0 flex items-center bottom-0 h-[318px] hover:bg-linear-to-b hover:from-transparent via-black/50 to-transparent text-6xl"
+            animate={
+              atEnd === "right"
+                ? {
+                    opacity: 0,
+                  }
+                : {
+                    opacity: 1,
+                  }
+            }
+            onClick={scrollRight}
+          >
+            <TbChevronCompactRight />
+          </motion.div>
         </div>
       </div>
     </div>
