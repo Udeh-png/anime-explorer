a84068bfc819dca131007c78f64ebabcdd7e301e
Udeh-png <leonwokedichisom@gmail.com>
2025-09-26 20:02:16 +0100

    Made character details page responsive

--- src/app/[id]/[title]/[charId]/page.tsx
+++ src/app/[id]/[title]/[charId]/page.tsx
@@ -32,7 +32,7 @@ export default async function ({
   return (
     <div className="">
       <div className="p-5">
-        <div className="flex items-center">
+        <div className="flex items-end">
           <div className="w-auto h-auto">
             <Image
               src={image.large}
@@ -42,15 +42,15 @@ export default async function ({
             />
           </div>
 
-          <div className="w-full ml-5 flex flex-col justify-between">
+          <div className="w-full min-[600px]:ml-5 ml-2 flex flex-col justify-between">
             <div className="">
-              <p className="text-4xl font-semibold">{name.full}</p>
-              <p className="text-2xl">{name.native}</p>
-              <p className="text-accent-two drop-shadow-accent-two w-fit rounded-full">
+              <p className="min-[600px]:text-4xl font-semibold">{name.full}</p>
+              <p className="min-[600px]:text-2xl text-sm">{name.native}</p>
+              <p className="min-[600px]:text-base text-xs text-accent-two drop-shadow-accent-two w-fit rounded-full">
                 {role}
               </p>
             </div>
-            <div className="text-sm text-white/80 mt-2">
+            <div className="min-[600px]:text-sm min-[600px]:block hidden text-[10px] text-white/80 min-[600px]:mt-2">
               {age && (
                 <p>
                   Age: <span className="underline">{age}</span>
@@ -88,13 +88,47 @@ export default async function ({
           </div>
         </div>
 
+        <div className="min-[600px]:text-sm text-[10px] text-white/80 block min-[600px]:hidden mt-2">
+          {age && (
+            <p>
+              Age: <span className="underline">{age}</span>
+            </p>
+          )}
+          {gender && (
+            <p>
+              Gender: <span className="underline">{gender}</span>
+            </p>
+          )}
+          {bloodType && (
+            <p>
+              Blood-Type: <span className="underline">{bloodType}</span>{" "}
+            </p>
+          )}
+          {name.alternative.length > 0 && (
+            <p>
+              Alternative {name.alternative.length > 1 ? "Names" : "Name"}:{" "}
+              <span className="underline">
+                {name.alternative.map((alt, i) => {
+                  if (i > 0) {
+                    return `, ${alt}`;
+                  }
+                  return alt;
+                })}
+              </span>
+            </p>
+          )}
+          {month && day && (
+            <p>
+              Date of birth: <span className="underline">{dob}</span>
+            </p>
+          )}
+        </div>
+
         <div className="mt-3">
           <p className="text-xl font-semibold">Voice Actors</p>
           <div className="py-2 overflow-auto px-3 w-fit max-w-130 max-h-40 grid grid-cols-2 gap-x-10 gap-y-7 items-start scrollable">
             {voiceActors.map((voiceActor, i) => {
               const { image, name, languageV2 } = voiceActor;
-              console.log(languageV2);
-
               return (
                 <div className="flex gap-x-2 items-center" key={i}>
                   <div className="overflow-clip rounded-[1px]">
@@ -107,8 +141,10 @@ export default async function ({
                     ></Image>
                   </div>
                   <div>
-                    <p>{name.full}</p>
-                    <p className="text-white/40 text-xs">{languageV2}</p>
+                    <p className="text-[10px] min-[600px]:text-base">
+                      {name.full}
+                    </p>
+                    <p className="text-white/40 text-[10px]">{languageV2}</p>
                   </div>
                 </div>
               );
