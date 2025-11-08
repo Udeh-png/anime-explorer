a70b71046b095166064751fb0f25ef55f068db20
Udeh-png <leonwokedichisom@gmail.com>
2025-11-08 09:49:59 +0100

    start redesigning home page; start home page hero redesign

--- src/components/home/slideContent.tsx
+++ src/components/home/slideContent.tsx
@@ -1,119 +1,45 @@
-"use client";
-
-import { Media } from "@/types";
-import { formatTitle, getStarCount } from "@/utils/sharedUtils";
-import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import { FaPlay, FaPlus } from "react-icons/fa6";
 import {
-  TiStarFullOutline,
-  TiStarHalfOutline,
-  TiStarOutline,
-} from "react-icons/ti";
+  FaPlay,
+  FaRegBookmark,
+  FaRegDotCircle,
+  FaRegHeart,
+} from "react-icons/fa";
 
-export const SlideContent = ({ media }: { media: Media }) => {
-  const {
-    bannerImage,
-    coverImage,
-    streamingEpisodes,
-    id,
-    title,
-    averageScore,
-    description,
-  } = media;
-  const { emptyStars, fullStars, hasHalfStars } = getStarCount(averageScore, 5);
-  const firstEpUrl = streamingEpisodes[0] ? streamingEpisodes[0].url : "";
-  const streamLink = firstEpUrl.slice(0, firstEpUrl.lastIndexOf("/"));
-
-  const mediaTitle = title.english || title.romaji;
-  const desktopImage = bannerImage || coverImage.extraLarge;
-  const mobileImage = coverImage.extraLarge;
-  const [isMobile, setIsMobile] = useState(false);
-  const imageUrl = isMobile ? mobileImage : desktopImage;
-
-  const descRef = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    setIsMobile(navigator.userAgent.toLowerCase().includes("mobi"));
-    const desc = descRef.current;
-    if (desc) {
-      desc.innerHTML = description;
-    }
-  }, []);
+export const HeroSliderContent = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
   return (
-    <div
-      onClick={() => {
-        console.log("hi");
-      }}
-      className="mx-auto relative overflow-clip"
-      style={{
-        maskImage: "linear-gradient(to bottom, black 70%, transparent",
-      }}
-    >
-      <div className="absolute inset-0 bg-linear-to-r from-black/40 from-10% to-transparent to-50% z-5"></div>
-      <div className={`relative min-[1090px]:h-110 h-120 overflow-clip`}>
-        <Image
-          src={imageUrl}
-          alt=""
-          fill
-          sizes="2920px"
-          className="object-cover"
-        />
-        <div className="absolute min-[1090px]:top-10 top-5 min-[1090px]:left-25 left-4 z-10 space-y-2">
-          <p className="min-[1090px]:text-4xl text-2xl font-semibold line-clamp-2 max-w-2xl">
-            {mediaTitle}
-          </p>
-          <div className="flex text-yellow-300 min-[1090px]:text-2xl text-lg">
-            {Array.from({ length: fullStars }).map((_, i) => (
-              <TiStarFullOutline key={i} />
-            ))}
-            {hasHalfStars && <TiStarHalfOutline />}
-            {Array.from({ length: emptyStars }).map((_, i) => (
-              <TiStarOutline key={i} />
-            ))}
-          </div>
-          <div className="flex gap-2 max-w-full flex-wrap">
-            {media.genres.map((genre, i) => (
-              <p
-                key={i}
-                className="px-2 py-0.5 bg-background/50 rounded-full text-xs min-[1090px]:text-sm"
-              >
-                {genre}
-              </p>
-            ))}
-          </div>
-          <div className=" w-sm">
-            <p
-              ref={descRef}
-              className="line-clamp-5 text-white/90 mb-1 min-[1090px]:[display:-webkit-box] hidden"
-            ></p>
-          </div>
-          <div className="flex min-[1090px]:gap-x-3 gap-x-2">
-            {firstEpUrl && (
-              <Link
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-                href={streamLink}
-                target="_blank"
-                className="min-[1090px]:px-5 min-[1090px]:py-2 px-2 py-2 bg-blue-400 min-[1090px]:text-base text-sm rounded-lg flex items-center w-fit gap-x-2"
-              >
-                <FaPlay />
-                <span>Watch Now</span>
-              </Link>
-            )}
-            <button
-              className="min-[1090px]:px-5 min-[1090px]:py-2 px-2 py-2 border-2 min-[1090px]:text-base text-sm rounded-lg flex items-center w-fit gap-x-2 cursor-pointer"
-              onClick={(e) => {
-                e.stopPropagation();
-                console.log("ola");
-              }}
-            >
-              <FaPlus className="text-lg" />
-              <span>Add To Watchlist</span>
+    <div className="pt-20 pl-15 h-full">
+      <div className="max-w-md">
+        <p className="font-bold text-7xl mb-5">{title}</p>
+        <div className="text-xs text-white/60 font-light flex gap-x-2 items-center mb-3">
+          <span>Sub | Dub </span>
+          <FaRegDotCircle className="text-[8px]" />
+          <span className="italic">Action, Fantasy, Shonen</span>
+        </div>
+        <p className="text-sm text-white/60 font-light line-clamp-4 mb-9 leading-relaxed">
+          {description}
+        </p>
+
+        <div className="flex gap-x-2.5">
+          <Link
+            href={"/"}
+            className="flex items-center text-sm gap-x-2 px-4 bg-blue-500 uppercase"
+          >
+            <FaPlay className="" />
+            Start Watching EP 1
+          </Link>
+          <div className="grid grid-cols-2 gap-x-2.5 w-23 h-10 text-xl">
+            <button className="border-2 border-blue-500 text-blue-500 flex justify-center items-center cursor-pointer">
+              <FaRegBookmark />
+            </button>
+            <button className="border-2 border-blue-500 text-blue-500 flex justify-center items-center cursor-pointer">
+              <FaRegHeart />
             </button>
           </div>
         </div>
--- src/sections/home/Hero.tsx
+++ src/sections/home/Hero.tsx
@@ -1,35 +1,107 @@
 "use client";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper/modules";
-import { PageObject } from "@/types";
-import { SlideContent } from "@/components/home/slideContent";
-import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import { HeroSliderContent } from "@/components/home/slideContent";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+import { Swiper as Swipe, SwiperSlide } from "swiper/react";
 
-export const Hero = ({ pageObjs }: { pageObjs: PageObject }) => {
+export const Hero = () => {
   return (
-    <div className="">
-      <Swiper
-        className="min-h-fit pb-5! caret-transparent"
-        modules={[Autoplay, Navigation]}
+    <div className="caret-transparent h-185">
+      <Swipe
+        className="h-full"
+        modules={[Navigation, Autoplay, EffectFade, Pagination]}
+        effect="fade"
+        speed={1100}
+        fadeEffect={{
+          crossFade: true,
+        }}
+        loop
+        pagination={{
+          el: ".el",
+          clickable: true,
+          renderBullet: (index, className) => {
+            console.log(className);
+
+            return `
+              <div class="${className} custom-bullet" data-index="${index}">
+                <div class="bullet-progress"></div>
+              </div>
+            `;
+          },
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
         navigation={{
+          enabled: true,
+          addIcons: true,
           nextEl: ".nextEl",
           prevEl: ".prevEl",
         }}
-        autoplay={{
-          pauseOnMouseEnter: true,
-        }}
-        loop
-        slidesPerView={1}
       >
-        <BiChevronRight className="nextEl absolute right-0 top-1/2 z-10 text-7xl -translate-y-1/2 min-[1090px]:block hidden" />
-        <BiChevronLeft className="prevEl absolute left-0 top-1/2 z-10 text-7xl -translate-y-1/2 min-[1090px]:block hidden" />
-        {pageObjs.media.map((media, i) => (
-          <SwiperSlide key={i}>
-            <SlideContent media={media} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+        <SwiperSlide>
+          <HeroSliderContent
+            title="Gachiakuta"
+            description="
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi esse
+          quae assumenda impedit odio velit hic magnam consequatur omnis,
+          laboriosam libero laborum incidunt. Et ab optio similique iste
+          inventore, nam suscipit voluptatem? Amet error voluptatem illo et ut,
+          similique reprehenderit vero nostrum excepturi placeat est porro
+          suscipit deserunt, quaerat assumenda?
+        "
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <HeroSliderContent
+            title="One Piece"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. In quis sed repellendus nisi quo tempora cumque facere voluptatum doloremque, ipsum, fugit nostrum odit qui placeat inventore repellat eos temporibus delectus alias, modi ab deleniti. Ut sint nisi enim, placeat veniam modi quo vero cumque corrupti repudiandae id pariatur recusandae ipsum?"
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <HeroSliderContent
+            title="Death Note"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed minima vero asperiores officiis fugit tempore. Dolorem impedit voluptatum ducimus aperiam delectus, eum corrupti nisi iure magnam modi magni enim placeat optio vero possimus architecto aliquid molestias minima itaque explicabo quibusdam? Minus aliquid quaerat, ex omnis pariatur quam molestiae at cumque."
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <HeroSliderContent
+            title="Naruto"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum consequatur dignissimos neque ipsum, sed voluptatem enim corrupti porro! Nam nihil quia doloremque. Eum quis explicabo adipisci accusantium, iste soluta. Autem neque officiis asperiores facere nam obcaecati libero. Quibusdam alias neque possimus earum minima itaque placeat, laudantium inventore omnis fugit accusantium.
+          "
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <HeroSliderContent
+            title="Dandadan"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut explicabo perferendis odio autem, dolorem adipisci eaque sed quis quae repellendus sint, omnis impedit placeat nam dolorum. Odit quod commodi doloremque officiis cumque quae consequatur mollitia suscipit aliquam ipsum cupiditate soluta vero in esse quisquam, voluptatum quidem doloribus sequi maxime! Doloribus.
+          "
+          />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <HeroSliderContent
+            title="Marshel"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, perferendis? Quis quo nostrum exercitationem placeat iure obcaecati repudiandae sint officiis suscipit, consectetur atque. Labore ipsam ducimus numquam aspernatur deleniti voluptate minus reiciendis veniam? Consequuntur tenetur incidunt numquam? Delectus nihil obcaecati accusantium labore? Voluptatem error quod, autem officia quis temporibus nemo?"
+          />
+        </SwiperSlide>
+
+        <div className="w-15 absolute z-10 left-0 top-0 bottom-0 flex items-center justify-center text-5xl">
+          <MdChevronLeft className="prevEl cursor-pointer absolute top-50" />
+        </div>
+        <div className="w-15 absolute z-10 right-0 top-0 bottom-0 flex items-center justify-center text-5xl">
+          <MdChevronRight className="nextEl cursor-pointer absolute top-50" />
+        </div>
+        <div className="flex space-x-2! items-center mt-14 max-w-md pl-15 el absolute bottom-78 z-10"></div>
+      </Swipe>
     </div>
   );
 };
+// movieName: the mummy
+// h-185
